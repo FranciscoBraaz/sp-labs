6fc82e4c4317c8b481c62c46476754ad42e7ed5e
FranciscoBraaz <francisco_braaz@hotmail.com>
2022-05-25 17:21:34 -0300

    refactor(CaseSection): refatora css para sass

--- src/components/CasesSection/index.jsx
+++ src/components/CasesSection/index.jsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import api from '../../services/api';
 import { ReactComponent as ArrowRight } from '../../assets/images/arrow-right.svg';
-import './styles.css';
+import './styles.scss';
 import { CaseSkeleton } from '../CaseSkeleton';
 
 function Case({ title, description }) {
   return (
-    <div className="cases__item">
-      <h4 className="cases__item-title">{title}</h4>
-      <p className="cases__item-description">{description}</p>
-      <button className="cases__item-button">
+    <div className="case">
+      <h4 className="case__title">{title}</h4>
+      <p className="case__description">{description}</p>
+      <button className="case__button">
         Acesse
         <ArrowRight />
       </button>
@@ -54,7 +54,7 @@ export function CasesSection() {
   return (
     <section id="casesSection" className="cases">
       <div className="container">
-        <div className="cases__container-title">
+        <div className="cases__header">
           <h2 className="cases__title">
             Problemas complexos. <br />
             Soluções <span className="cases__strike">complexas</span>{' '}
