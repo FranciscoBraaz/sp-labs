f7cb747fc2cbd1d930970a10cee6ba215d4e4da8
FranciscoBraaz <francisco_braaz@hotmail.com>
2022-05-25 22:34:30 -0300

    fix(api): remove comentário

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,5 @@
 import axios from 'axios';
 
-// let url;
-
-// if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-//   url = 'https://sp-labs.vercel.app/api';
-// } else {
-//   url = 'https://sp-labs.vercel.app/api';
-// }
-
 const api = axios.create({
   baseURL: 'https://sp-labs.vercel.app/api',
 });
